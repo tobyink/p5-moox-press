410b9a3afdccaa048d9c0b78d823bb39e8ef6729
Toby Inkster <mail@tobyinkster.co.uk>
2019-10-23 20:02:30 +0100

    oops, bump version

--- lib/MooX/Press.pm
+++ lib/MooX/Press.pm
@@ -5,7 +5,7 @@ use warnings;
 package MooX::Press;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.003';
+our $VERSION   = '0.004';
 
 use Exporter::Tiny qw(mkopt);
 use Scalar::Util qw(blessed);
