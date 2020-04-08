f6b800d995d42c8e19fee99bf286a896f0a83940
Toby Inkster <mail@tobyinkster.co.uk>
2020-04-08 10:35:34 +0100

    bump version; changelog

--- lib/MooX/Press.pm
+++ lib/MooX/Press.pm
@@ -5,7 +5,7 @@ use warnings;
 package MooX::Press;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.057';
+our $VERSION   = '0.058';
 
 use Types::Standard 1.010000 -is, -types;
 use Types::TypeTiny qw(ArrayLike HashLike);
