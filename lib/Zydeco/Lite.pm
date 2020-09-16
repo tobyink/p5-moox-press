ee37deeaea80fe8668e324bc440328712963beb1
Toby Inkster <mail@tobyinkster.co.uk>
2020-09-16 14:20:02 +0100

    bump version; changelog

--- lib/Zydeco/Lite.pm
+++ lib/Zydeco/Lite.pm
@@ -5,7 +5,7 @@ use warnings;
 package Zydeco::Lite;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.067';
+our $VERSION   = '0.068';
 
 use MooX::Press ();
 use Types::Standard qw( -types -is );
